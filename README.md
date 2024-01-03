348b50b367ecc6eb7f4abe80eb4a77af68986dc3
jmurzuar <jmurzuar@gmail>
2024-01-03 01:23:39 -0300

    actualización 03.01

---
+++ README.md
@@ -0,0 +1,92 @@
+# PROYECTO 2: Aplicación CRUD
+
+## **ÍNDICE**
+
+* [1. Intro](#1-intro)
+* [2. Estructura de la página](#2-Estructura)
+* [3. Pantallas](#3-Pantallas)
+* [4. Tecnologías Utilizadas](#4-Tecnologías-Utilizadas)
+* [5. Principales Características](#5-Principales-Características)
+* [6. Legal](#6-Legal)
+  
+****
+
+## 1. Intro
+
+Sitio estático desarrollado en HTML5, CSS y responsive, el ejemplo corresponde a una radio online 
+
+[volver al índice](#ÍNDICE)
+****
+
+## 2. Estructura
+
+![estructura](https://github.com/jmurzuar/PY1-Landing-Venta/assets/84281899/707d98c0-4aa7-4a61-b64e-9351adf1189f)
+
+
+[volver al índice](#ÍNDICE)
+****
+
+## 3. Pantallas
+![pantalla1](https://github.com/jmurzuar/PY1-Landing-Venta/assets/84281899/c199114b-562b-4b64-b51e-e7a800ed515a)
+![pantalla2](https://github.com/jmurzuar/PY1-Landing-Venta/assets/84281899/a6ac07a8-01a9-4400-821c-b952497b69d1)
+![pantalla3](https://github.com/jmurzuar/PY1-Landing-Venta/assets/84281899/0c945321-9a40-453f-bcdc-4e52fa14602d)
+![pantalla4](https://github.com/jmurzuar/PY1-Landing-Venta/assets/84281899/b26bc1a3-57b8-4f65-8516-8ff50ebf9e90)
+
+
+[volver al índice](#ÍNDICE)
+****
+
+## 4. Tecnologías Utilizadas
+
+![losgos_tec](https://github.com/jmurzuar/PY1-Landing-Venta/assets/84281899/f978afc9-5e20-4b70-9c1d-bc493cbcf056)
+
+
+El objetivo principal de este proyecto fue desarrollar una interfaz utilizando HTML y CSS, en toda su aplicación.
+
+Aplicando:
+
+- Prototipado simple (baja findelidad) que muestra el plan de desarrollo. utilizando diseños sensillos, que muestran la página que vas a construir.
+- Etiquetas estándar HTML5.
+- CSS, incluyendo técnicas Flexbox, CSS Grids y Responsive Web Design.
+- Se utiliza Bootstrap
+- Se realiza control de versiones con GIT y GITHUB
+
+[volver al índice](#ÍNDICE)
+****
+
+## 5. Principales Características
+
+Usa esta lista se encuentran los requisitos mínimos del proyecto fueron:
+
+### HTML - CSS
+
+- [X] Realizado de manera individual
+- [X] Desarrollar prototipado simple.
+- [X] Aplicar en todo el sitio HTML semántico de estándar no.5 (HTML5).
+- [X] Aplicar tipos de selectores en CSS.
+- [X] Sección `Header`, `Jumbotron`, `footer`
+- [X] Sección de registro de usuario
+- [X] Sección artículos o catálogo
+- [X] Responsive Web Design (Vista en móviles con uso de media queries)
+
+### GIT y GITHUB
+- [X] Utilizar GIT para en control de versiones.
+- [X] Crear un repositorio en GitHub y realizar mínimo 5 "commits" en tu repositorio.
+
+### Publicación y difusión 
+- [X] Crear una URL para compartir (GitHub Pages)
+- [X] Se crea archivo README con la descripción dentro del repositorio
+
+### Fecha de entrega
+- [X] Entregado el Lunes 27.11.2023
+
+[volver al índice](#ÍNDICE)
+****
+
+## 6. Legal
+
+- Esta página fur creada con fines educativos, NO COMERCIAL, contiene imagenes con derechos de autor que se han utilizado solo para ejemplos
+- Página creada en el marco del Bootcamp desarrollo web full stack, corte 11 de la Universidad de Desarrrollo - Chile
+- Creado en Noviembre 2023
+  
+[volver al índice](#ÍNDICE)
--- app.js
+++ app.js
@@ -1,59 +1,91 @@
 let cards = [];
 const form = document.getElementById('cardForm');
 
-function carga_fecha_combo(){
-const fechaActual = new Date().toISOString().split('T')[0];
+function carga_fecha_combo() {
+  const fechaActual = new Date().toISOString().split('T')[0];
 
-    // Establecer la fecha mínima en el elemento input
-    const fechaInput = document.getElementById('fechavence');
-    fechaInput.min = fechaActual;
-    fechaInput.value = fechaActual
+  // Establecer la fecha mínima en el elemento input
+  const fechaInput = document.getElementById('fechavence');
+  fechaInput.min = fechaActual;
+  fechaInput.value = fechaActual
 
 }
 
 // Cargar datos del localStorage al iniciar
 function loadFromLocalStorage() {
-    const storedData = localStorage.getItem('cards');
-    if (storedData) {
-      cards = JSON.parse(storedData);
-      sortCardsByTimestamp(); // Ordenar las tarjetas al cargar
-      renderCards();
-    }
+  const storedData = localStorage.getItem('cards');
+  if (storedData) {
+    cards = JSON.parse(storedData);
+    sortCardsByTimestamp(); // Ordenar las tarjetas al cargar
+    renderCards();
   }
+}
 
-  function contar_registros(){
-    
-    if (typeof(Storage) !== "undefined") {
-      // Obtener el contenido de 'cards' desde localStorage
-      var cardsData = localStorage.getItem('cards');
-    
-      // Verificar si 'cards' tiene algún contenido
-      if (cardsData) {
-        // Parsear el contenido como JSON para obtener un array u objeto
-        var cardsArray = JSON.parse(cardsData);
-    
-        // Obtener el número de registros en 'cards'
+function contar_registros() {
+
+  if (typeof (Storage) !== "undefined") {
+    // Obtener el contenido de 'cards' desde localStorage
+    var cardsData = localStorage.getItem('cards');
+
+    // Verificar si 'cards' tiene algún contenido
+    if (cardsData) {
+      // Parsear el contenido como JSON para obtener un array u objeto
+      var cardsArray = JSON.parse(cardsData);
+
+      // Obtener el número de registros en 'cards'
         var numRegistros = cardsArray.length;
+
+        // Filtrar elementos basados en algún criterio (por ejemplo, una propiedad llamada 'tipo' igual a 'someValue')
+        var filtro = 'Cerrado'; // Ajusta esto según tus necesidades
+         var filtrados = cardsArray.filter(function(card) {
+          return card.estado === filtro;
+        });
+
+  // Obtener el número de registros filtrados
+        var numRegistrosFiltrados = filtrados.length;
+
+             // Filtrar elementos basados en algún criterio (por ejemplo, una propiedad llamada 'tipo' igual a 'someValue')
+             var filtro2 = 'Programado'; // Ajusta esto según tus necesidades
+             var filtrados2 = cardsArray.filter(function(card2) {
+              return card2.estado === filtro2;
+            });
     
-        // Mostrar el número de registros en la consola
-        //console.log("Número de registros en 'cards': " + numRegistros);
-        document.getElementById('card_contar').textContent = "Número de registros encontrados: " + numRegistros;
-      } else {
-        document.getElementById('card_contar').textContent = "No se encontraron registros";
-        //console.log("No hay datos en 'cards' en localStorage.");
-      }
+      // Obtener el número de registros filtrados
+            var numRegistrosFiltrados2 = filtrados2.length;
+        
+
+              // Obtener el número de registros filtrados
+        var numRegistrosFiltrados = filtrados.length;
+
+        // Filtrar elementos basados en algún criterio (por ejemplo, una propiedad llamada 'tipo' igual a 'someValue')
+        var filtro3 = 'Ingresado'; // Ajusta esto según tus necesidades
+        var filtrados3 = cardsArray.filter(function(card3) {
+         return card3.estado === filtro3;
+       });
+
+ // Obtener el número de registros filtrados
+       var numRegistrosFiltrados3 = filtrados3.length;
+   
+
+      // Mostrar el número de registros en la consola
+      //console.log("Número de registros en 'cards': " + numRegistros);
+      document.getElementById('card_contar').innerHTML = '<p>&nbsp;&nbsp;Total registros: ' + numRegistros + ' (Cerrados: '+ numRegistrosFiltrados +', Progamados: '+ numRegistrosFiltrados2 + ', Ingresados: ' + numRegistrosFiltrados3 + ')</p>';
     } else {
-      document.getElementById('card_contar').textContent = "El navegador no es compatible con localStorage";
-      //console.log("El navegador no es compatible con localStorage.");
+      document.getElementById('card_contar').innerHTML = '<p>&nbsp;&nbsp;&nbsp;&nbsp;No se encontraron registros</p>';
+      //console.log("No hay datos en 'cards' en localStorage.");
     }
-
+  } else {
+    document.getElementById('card_contar').innerHTML = '<p>&nbsp;&nbsp;&nbsp;&nbsp;El navegador no es compatible con localStorage</p>';
+    //console.log("El navegador no es compatible con localStorage.");
   }
 
-  
-  // Guardar datos en localStorage
-  function saveToLocalStorage() {
-    localStorage.setItem('cards', JSON.stringify(cards));
-  }
+}
+
+
+// Guardar datos en localStorage
+function saveToLocalStorage() {
+  localStorage.setItem('cards', JSON.stringify(cards));
+}
 
 
 
@@ -71,7 +103,7 @@ function addCard() {
       fecha: cardfechavence,
       estado: cardestado,
       timestamp: new Date().toLocaleString(),
-    
+
 
     };
 
@@ -88,51 +120,55 @@ function addCard() {
 
 // diplicado con listener
 
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function (event) {
 
-    //elimina comportamientos por defecto del formulario
-        event.preventDefault();
-        
-        const cardContent = document.getElementById('cardContent').value;
-        const cardContent2 = document.getElementById('cardContent2').value;
-        const cardfechavence = document.getElementById('fechavence').value;
-        const cardestado = document.getElementById('estado').value;
+  //elimina comportamientos por defecto del formulario
+  event.preventDefault();
 
-          
-      if (cardContent.trim() !== '' && cardContent2.trim() !== '') {
-        const card = {
-          id: Date.now(),
-          content: cardContent,
-          content2: cardContent2,
-          fecha: cardfechavence,
-          estado: cardestado,
-          timestamp: new Date().toLocaleString(),
-        
-    
-        };
-    
-        cards.push(card);
-        sortCardsByTimestamp(); // Ordenar las tarjetas después de añadir una nueva
-        renderCards();
-        saveToLocalStorage(); // Guardar datos en localStorage
-        // document.getElementById('cardContent').value = '';
-        // document.getElementById('cardContent2').value = '';
-        form.reset();
-        carga_fecha_combo();
-        contar_registros();
-               
-        
+  const cardContent = document.getElementById('cardContent').value;
+  const cardContent2 = document.getElementById('cardContent2').value;
+  const cardfechavence = document.getElementById('fechavence').value;
+  const cardestado = document.getElementById('estado').value;
 
-    }else{
-    
-    alert ('Favor llenar todos los campos');
 
-    }
-  
+  if (cardContent.trim() !== '' && cardContent2.trim() !== '') {
+    const card = {
+      id: Date.now(),
+      content: cardContent,
+      content2: cardContent2,
+      fecha: cardfechavence,
+      estado: cardestado,
+      timestamp: new Date().toLocaleString(),
+
+
+    };
+
+    document.getElementById("cardForm").style.backgroundColor = "#eef3f5";
+    document.getElementsByClassName("button")[0].innerHTML = "Grabar";
+    document.getElementsByClassName("button")[0].style.backgroundColor = "#7fffd4"
+
+    cards.push(card);
+    sortCardsByTimestamp(); // Ordenar las tarjetas después de añadir una nueva
+    renderCards();
+    saveToLocalStorage(); // Guardar datos en localStorage
+    // document.getElementById('cardContent').value = '';
+    // document.getElementById('cardContent2').value = '';
+    form.reset();
+    carga_fecha_combo();
+    contar_registros();
+
+
+
+  } else {
+
+    alert('Favor llenar todos los campos');
+
+  }
+
 }
 
-    )
- 
+)
+
 // fin listener
 
 
@@ -147,22 +183,22 @@ function deleteCard(id) {
 
 // orden numérico / fecha - hora
 function sortCardsByTimestamp() {
-    //cards.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
-    cards.sort((b, a) => new Date(b.fecha) - new Date(a.fecha));
+  //cards.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+  cards.sort((b, a) => new Date(b.fecha) - new Date(a.fecha));
 }
 
 // orden alfabético
 function sortCardsAlphabetically() {
-    cards.sort((a, b) => a.content.localeCompare(b.content));
-  }
+  cards.sort((a, b) => a.content.localeCompare(b.content));
+}
 
 
 function renderCards() {
   const cardContainer = document.getElementById('cardContainer');
-  
+
   cardContainer.innerHTML = '';
 
-  
+
   cards.forEach(card => {
 
     var cardFecha = new Date(card.fecha);
@@ -170,26 +206,26 @@ function renderCards() {
     hoy.setHours(0, 0, 0, 0);
     hoy.setDate(hoy.getDate() - 1);
 
-    if (cardFecha>=hoy && card.estado!='Cerrado'){
+    if (cardFecha >= hoy && card.estado != 'Cerrado') {
       fechas = 'vence';
-    } else if (cardFecha<hoy && card.estado!='Cerrado'){
+    } else if (cardFecha < hoy && card.estado != 'Cerrado') {
       fechas = 'vencido';
     }
-    else{
+    else {
       fechas = '';
     }
 
-    if (card.estado=='Ingresado'){
+    if (card.estado == 'Ingresado') {
       imagen_estado = '<img class="logo_estado" src="images/ingresado.png" width="25" height="20">';
-    }else if (card.estado=='Cerrado'){
+    } else if (card.estado == 'Cerrado') {
       imagen_estado = '<img class="logo_estado" src="images/cerrado.png" width="15" height="20">';
     }
-    else{
+    else {
       imagen_estado = '<img class="logo_estado" src="images/calen.png" width="20" height="20">';
     }
-    
+
     const cardElement = document.createElement('div');
-    cardElement.className = 'card ' + card.estado +'';
+    cardElement.className = 'card ' + card.estado + '';
     cardElement.innerHTML = `
       <br>
       <p class="titulo1">${card.content}</p>
@@ -200,8 +236,8 @@ function renderCards() {
       <br>
       <br>
       <div class="div_botones">
-      <button class="button--secundary button" onclick="editCard(${card.id})">Editar</button>
-      <button class="button--terciary button" onclick="deleteCard(${card.id})">Eliminar</button>
+      <button class="button--secundary" onclick="editCard(${card.id})">Editar</button>
+      <button class="button--terciary" onclick="deleteCard(${card.id})">Eliminar</button>
       </div>
     `;
     cardContainer.appendChild(cardElement);
@@ -210,42 +246,49 @@ function renderCards() {
 
 
 function editCard(id) {
-    // Buscar la tarjeta con el ID específico
-
-    
-
-    const cardToEdit = cards.find(card => card.id === id);
-  
-    // Verificar si la tarjeta existe
-    if (cardToEdit) {
-      // Pedir al usuario la nueva información
-      //const newContent = prompt('Editar Contenido:', cardToEdit.content);
-      //const newContent2 = prompt('Editar Contenido2:', cardToEdit.content2);
-  
-      // Actualizar la tarjeta
-      //cardToEdit.content = newContent || cardToEdit.content;
-      //cardToEdit.content2 = newContent2 || cardToEdit.content2;
-      console.log(cardToEdit.content)
-      document.getElementById('cardContent').value = cardToEdit.content;
-      document.getElementById('cardContent2').value = cardToEdit.content2;
-      document.getElementById('fechavence').value = cardToEdit.fecha;
-      document.getElementById('estado').value = cardToEdit.estado;
-
-      const fechaInput = document.getElementById('fechavence');
-      fechaInput.min = cardToEdit.fecha;
-      
+  // Buscar la tarjeta con el ID específico
 
 
-      cards = cards.filter(card => card.id !== id);
-      cards.splice(id, 1);
-      // Renderizar y guardar en localStorage
-      sortCardsByTimestamp(); // Ordenar las tarjetas después de editar
-      renderCards();
-      saveToLocalStorage();
-      contar_registros();
 
-    }
+  const cardToEdit = cards.find(card => card.id === id);
+
+  // Verificar si la tarjeta existe
+  if (cardToEdit) {
+    // Pedir al usuario la nueva información
+    //const newContent = prompt('Editar Contenido:', cardToEdit.content);
+    //const newContent2 = prompt('Editar Contenido2:', cardToEdit.content2);
+
+    // Actualizar la tarjeta
+    //cardToEdit.content = newContent || cardToEdit.content;
+    //cardToEdit.content2 = newContent2 || cardToEdit.content2;
+    //console.log(cardToEdit.content)
+
+    document.getElementById('cardContent').value = cardToEdit.content;
+    document.getElementById('cardContent2').value = cardToEdit.content2;
+    document.getElementById('fechavence').value = cardToEdit.fecha;
+    document.getElementById('estado').value = cardToEdit.estado;
+
+    const fechaInput = document.getElementById('fechavence');
+    fechaInput.min = cardToEdit.fecha;
+
+    document.getElementById("cardForm").style.backgroundColor = "#f8bf02";
+    document.getElementsByClassName("button")[0].innerHTML = "Editar";
+    document.getElementsByClassName("button")[0].style.backgroundColor = "#b7f312"
+
+
+
+    cards = cards.filter(card => card.id !== id);
+    cards.splice(id, 1);
+    // Renderizar y guardar en localStorage
+    sortCardsByTimestamp(); // Ordenar las tarjetas después de editar
+    renderCards();
+    saveToLocalStorage();
+    contar_registros();
+
+
+
   }
+}
 
 
 
